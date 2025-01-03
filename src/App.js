964fc60ccf9ade4af77a135c98334b454a7d4b8d
Eylül Özatman <74192529+eylulozatman@users.noreply.github.com>
2025-01-03 22:28:51 +0300

    route change

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import MainPage from './mainPage';
-import AdminPage from './admin';
-import HomePage from './homepage';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+
+// Sayfalar
+import MainPage from "./MainPage"; // Homepage
+import AdminPage from "./AdminPage"; // Admin sayfası
+import HomePage from "./HomePage"; // Bu sayfa varsayılan olarak kullanıcıya gösterilecek
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/admin" element={<AdminPage />} />
+        {/* Anasayfa: Kullanıcı ilk buraya gelecek */}
+        <Route path="/" element={<HomePage />} />
+
+        {/* AdminPage'e gitmeye çalışanlar, AdminPage'i görmeyecek ve anasayfaya yönlendirilecek */}
+        <Route path="/admin" element={<Navigate to="/" />} /> {/* Admin sayfası yönlendirilerek engelleniyor */}
+        
+        {/* Homepage'e gitmek istenirse, kullanıcı buraya yönlendirilir */}
         <Route path="/homepage" element={<HomePage />} />
       </Routes>
     </Router>
